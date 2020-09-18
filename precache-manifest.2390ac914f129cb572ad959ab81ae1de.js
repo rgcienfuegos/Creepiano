fa44145eb478dc3ba7649a01f1fc54e8c7ed777c
rgcienfuegos <rgcienfuegos@hotmail.com>
2020-09-18 21:53:57 +0200

    Updates

---
+++ precache-manifest.2390ac914f129cb572ad959ab81ae1de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa40161c90be68490b559625438d850e",
+    "url": "/creepiano/index.html"
+  },
+  {
+    "revision": "86b0807f975a846a5b57",
+    "url": "/creepiano/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b2bffea16f79502b656",
+    "url": "/creepiano/static/js/2.f937277c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/creepiano/static/js/2.f937277c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b0807f975a846a5b57",
+    "url": "/creepiano/static/js/main.8753b512.chunk.js"
+  },
+  {
+    "revision": "f26993ebc6088b3c478e",
+    "url": "/creepiano/static/js/runtime-main.7e191b38.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/creepiano/static/media/logo.5d5d9eef.svg"
+  }
+]);
